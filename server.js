551ee0175b37560b5dbff97eb6e7cf1efed28526
vibecastor <vibecastor@gmail.com>
2018-03-27 15:43:11 -0700

    server new get

--- server.js
+++ server.js
@@ -21,6 +21,13 @@ client.on('error', err => console.error(err));
 //api inputs
 
 app.get('/api/v1/books', (req, res) => res.send('It is alive!!!'));
+app.get('/api/v1/books', (req, res) => {
+  client.query(`
+  SELECT book_id, title, author, image_url FROM books`)
+    .then(result => res.send(result.rows))
+    .catch(console.error);
+});
+
 //this app.get needs more work once the database is operational.
 
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
