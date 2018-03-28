69577c42546edb6beb0d7ed89ed61044582a7494
vibecastor <vibecastor@gmail.com>
2018-03-27 17:57:00 -0700

    ready for deployment

--- server.js
+++ server.js
@@ -20,15 +20,15 @@ client.on('error', err => console.error(err));
 
 //api inputs
 
-app.get('/api/v1/books', (req, res) => res.send('It is alive!!!'));
+// app.get('/api/v1/books', (req, res) => res.send('It is alive!!!'));
+app.use(cors());
 app.get('/api/v1/books', (req, res) => {
   client.query(`
-  SELECT book_id, title, author, image_url FROM books`)
+  SELECT book_id, author, title, image_url FROM books`)
     .then(result => res.send(result.rows))
     .catch(console.error);
 });
 
-//this app.get needs more work once the database is operational.
 
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
 
